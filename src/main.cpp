64159d0a4ea40dc1becbb62068f8602a90fe3476
Jean-François Erdelyi <jferdelyi@gmail.com>
2022-07-11 01:01:00 +0200

    Initial commit
    Add the code running on ESP WROOM-32 with AZ-Delivery 0.96" OLED Display

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <Adafruit_SSD1306.h>
+#include <ArduinoHttpClient.h>
+#include <WiFi.h>
+
+// The BOOT button
+#define BTN_BOOT 0
+
+// Secrets
+#define SECRET_SSID "iPhone de Jean-François"
+#define SECRET_PASS "jaimelepate"
+
+// The display
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32);
+
+// Server data client
+String IP = String("http://zeta.local");
+String PORT = String("4242");
+String VERSION = String("v1");
+String GENERATE = String("generate");
+
+// Connection data
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, "172.20.10.2", 4242);
+bool requested = false;
+
+void initScreen() {
+	// Address 0x3C for 128x32
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+	display.setTextSize(1);
+	display.setTextColor(SSD1306_WHITE);
+	display.setCursor(0,0);
+	display.clearDisplay();
+}
+
+void initWiFi() {
+	// Set mode and disconnect previous connection
+	WiFi.mode(WIFI_STA);
+	WiFi.disconnect();
+	delay(100);
+
+	// New connection
+	WiFi.begin(SECRET_SSID, SECRET_PASS);
+	display.print("Conection to ");
+	display.println(SECRET_SSID);
+	display.display();
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+	}
+	display.println(WiFi.localIP());
+	display.display();
+}
+
+void setup() {
+	// Init screen
+	initScreen();
+
+	// Init WiFi
+	initWiFi();
+}
+
+void loop() {
+	// If the button BOOT is pressed
+	if(!requested && !digitalRead(BTN_BOOT)) {
+		// Block requests
+		requested=true;
+
+		// Init screen
+		display.clearDisplay();
+		display.setCursor(0,0);
+
+		// GET
+  		client.get("/" + VERSION + "/" + GENERATE);
+		int statusCode = client.responseStatusCode();
+		if(statusCode == 200) {
+			String response = client.responseBody();
+			display.println(response);
+		} else {
+			display.print("Error code ");
+			display.println(statusCode);
+		}
+
+		// Release
+		requested=false;
+	}
+
+	// Wait a bit
+	delay(10);
+  	yield();
+  	display.display();
+}
